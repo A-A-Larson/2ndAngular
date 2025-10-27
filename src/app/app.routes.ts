9604d579317682bca39c291fec75fd6b630c4583
drewdarko0 <65877684+drewdarko0@users.noreply.github.com>
2025-10-27 14:12:50 -0500

    Removed leftover boilerplate code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
-    { path: 'products', component: ProductListComponent },
-    { path: 'cart', component: CartComponent }
 ];
