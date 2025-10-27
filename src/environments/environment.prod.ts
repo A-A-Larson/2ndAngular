12ce43f6c2a1ddba0d364884d8e7bb92ae2db15a
Andrew Larson <65877684+A-A-Larson@users.noreply.github.com>
2025-10-27 14:21:52 -0500

    Remove API key from production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://trefle.io/api/v1', // use real remote API here
-  key: 'X-W0L2jeV5xjee9X4ucM2HmTt2WiiFN58IkeMXGJ40E'
+  key: ''
 };
